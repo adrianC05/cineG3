4092663949bfc200f992377830d2968e15aeb381
adrianC05 <aacarrion4@espe.edu.ec>
2023-12-04 14:30:42 -0500

    Nombre de integrantes

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Grupo N. 3</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
@@ -18,12 +18,15 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+                <h2>Integrantes</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <ul>
+                    <li>Adrian Carrion</li>
+                    <li>Juan Jacome</li>
+                    <li>Andres Chumo</li>
+                    <li>Johan Galeas</li>
+                </ul>
+
 
                 <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
